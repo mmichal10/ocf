48c8263664a4ea432016b68c5f9f9bfc9fa7c2d4
Adam Rutkowski <adam.j.rutkowski@intel.com>
2021-10-21 10:51:00 +0200

    failover test
    Signed-off-by: Adam Rutkowski <adam.j.rutkowski@intel.com>

---
+++ tests/functional/tests/management/test_failover.py
@@ -0,0 +1,32 @@
+from pyocf.types.cache import (
+    Cache,
+    CacheMode,
+    MetadataLayout,
+    CleaningPolicy,
+)
+from pyocf.types.volume import RamVolume
+from pyocf.types.volume_ocf import OCFVolume
+from pyocf.types.volume_replicated import ReplicatedVolume
+from pyocf.types.shared import (
+    OcfError,
+    OcfCompletion,
+    CacheLines,
+    CacheLineSize,
+    SeqCutOffPolicy,
+)
+from pyocf.utils import Size
+
+def test_setup_failover(pyocf_2_ctx):
+    ctx1 = pyocf_2_ctx[0]
+    ctx2 = pyocf_2_ctx[1]
+    mode = CacheMode.WO
+    cls = CacheLineSize.LINE_4KiB
+    prim_cache_vol = RamVolume(Size.from_MiB(35))
+    prim_core_vol = RamVolume(Size.from_MiB(100))
+    sec_cache_vol = RamVolume(Size.from_MiB(35))
+    sec_core_vol = RamVolume(Size.from_MiB(100))
+
+    cache = Cache.start_on_device(prim_cache_vol, ctx1, cache_mode=mode, cache_line_size=cls)
+
+
+
