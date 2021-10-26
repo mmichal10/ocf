44eca76e3ed2efd41b09da3617e728b1fcaaf7a2
Michal Mielewczyk <michal.mielewczyk@intel.com>
2021-10-26 10:53:35 +0200

    Add error messages on superblock mismatch
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@intel.com>

--- src/mngt/ocf_mngt_cache.c
+++ src/mngt/ocf_mngt_cache.c
@@ -2185,16 +2185,22 @@ static void _ocf_mngt_activate_check_superblock_complete(void *priv, int error)
 		OCF_PL_FINISH_RET(context->pipeline, result);
 
 	if (cache->conf_meta->metadata_layout != cache->metadata.layout) {
+		ocf_cache_log(cache, log_err, "Failed to activate failover instance: "
+				"invaild metadata layout\n");
 		OCF_PL_FINISH_RET(context->pipeline,
 				-OCF_ERR_METADATA_LAYOUT_MISMATCH);
 	}
 
 	if (cache->conf_meta->line_size != cache->metadata.line_size) {
+		ocf_cache_log(cache, log_err, "Failed to activate failover instance: "
+				"invaild cache line size\n");
 		OCF_PL_FINISH_RET(context->pipeline,
 				-OCF_ERR_CACHE_LINE_SIZE_MISMATCH);
 	}
 
 	if (cache->conf_meta->cleaning_policy_type != cache->cleaner.policy) {
+		ocf_cache_log(cache, log_err, "Failed to activate failover instance: "
+				"invaild cleaning policy\n");
 		OCF_PL_FINISH_RET(context->pipeline,
 				-OCF_ERR_CLEANING_POLICY_MISMATCH);
 	}
