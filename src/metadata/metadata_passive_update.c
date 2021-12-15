fba778ec748b202f9549d531199f1018c0ea388b
Michal Mielewczyk <michal.mielewczyk@intel.com>
2021-12-15 14:21:51 +0100

    Perf update v2

--- src/metadata/metadata_passive_update.c
+++ src/metadata/metadata_passive_update.c
@@ -143,30 +143,26 @@ static void handle_previously_valid(ocf_cache_t cache,
 }
 
 static inline void update_list_segment(ocf_cache_t cache,
-		ocf_cache_line_t start, ocf_cache_line_t count)
+		ocf_cache_line_t cline)
 {
-	ocf_cache_line_t cline, end;
+	ocf_part_id_t part_id;
 
-	for (cline = start, end = start + count; cline < end; cline++) {
-		ocf_part_id_t part_id;
+	metadata_clear_dirty_if_invalid(cache, cline);
+	metadata_clear_valid_if_clean(cache, cline);
 
-		metadata_clear_dirty_if_invalid(cache, cline);
-		metadata_clear_valid_if_clean(cache, cline);
-
-		part_id = ocf_metadata_get_partition_id(cache, cline);
-		switch (part_id) {
-			case PARTITION_FREELIST:
-				handle_previously_invalid(cache, cline);
-				break;
-			case PARTITION_DEFAULT:
-				handle_previously_valid(cache, cline);
-				break;
-			default:
-				ocf_cache_log(cache, log_crit, "Passive update: invalid "
-						"part id for cacheline %u: %hu\n", cline, part_id);
-				ENV_BUG();
-				break;
-		}
+	part_id = ocf_metadata_get_partition_id(cache, cline);
+	switch (part_id) {
+		case PARTITION_FREELIST:
+			handle_previously_invalid(cache, cline);
+			break;
+		case PARTITION_DEFAULT:
+			handle_previously_valid(cache, cline);
+			break;
+		default:
+			ocf_cache_log(cache, log_crit, "Passive update: invalid "
+					"part id for cacheline %u: %hu\n", cline, part_id);
+			ENV_BUG();
+			break;
 	}
 }
 
@@ -189,36 +185,155 @@ static void _dec_core_stats(ocf_core_t core)
 	ENV_BUG_ON(env_atomic_dec_return(part_dirty_counter) < 0);
 }
 
-static void cleanup_old_mapping(ocf_cache_t cache, ocf_cache_line_t start,
-		ocf_cache_line_t count)
+static void cleanup_old_mapping(ocf_cache_t cache, ocf_cache_line_t cline,
+		uint64_t core_line, ocf_core_id_t core_id)
 {
-	ocf_cache_line_t cline, end;
-	uint32_t lock_idx = ocf_metadata_concurrency_next_idx(cache->mngt_queue);
+	uint32_t lock_idx;
 
-	for (cline = start, end = start + count; cline < end; cline++) {
-		ocf_core_id_t core_id;
-		uint64_t core_line;
-		ocf_core_t core;
+	ENV_BUG_ON(core_id >= OCF_CORE_ID_INVALID);
+	ENV_BUG_ON(ocf_metadata_get_partition_id(cache, cline) !=
+			PARTITION_DEFAULT);
 
-		ocf_metadata_get_core_info(cache, cline, &core_id, &core_line);
+	_dec_core_stats(ocf_cache_get_core(cache, core_id));
 
-		ENV_BUG_ON(core_id > OCF_CORE_ID_INVALID);
+	lock_idx = ocf_metadata_concurrency_next_idx(cache->mngt_queue);
 
-		core = ocf_cache_get_core(cache, core_id);
-		if (!core)
-			continue;
+	ocf_hb_cline_prot_lock_wr(&cache->metadata.lock, lock_idx, core_id,
+			core_line);
+	ocf_metadata_remove_from_collision(cache, cline, PARTITION_DEFAULT);
+	ocf_hb_cline_prot_unlock_wr(&cache->metadata.lock, lock_idx, core_id,
+			core_line);
+}
 
-		ENV_BUG_ON(ocf_metadata_get_partition_id(cache, cline) !=
-				PARTITION_DEFAULT);
+/*
+static inline int find_diff(void *data, const void *collision,
+		size_t element_size, uint64_t *bitmap, uint32_t max_id)
+{
+	uint8_t i;
+	* Handle the last page of the collision section *
+	int max = OCF_MIN(COMPARE_AT_ONCE, max_id);
+	int result, diff;
 
-		_dec_core_stats(core);
+	for (i = 0; i < max; i++)
+	{
+		result = env_memcmp(data + element_size*i, element_size,
+				collision + element_size*i, element_size, &diff);
+		if (unlikely(result))
+			return -1;
 
-		ocf_hb_cline_prot_lock_wr(&cache->metadata.lock, lock_idx, core_id,
-				core_line);
-		ocf_metadata_remove_from_collision(cache, cline, PARTITION_DEFAULT);
-		ocf_hb_cline_prot_unlock_wr(&cache->metadata.lock, lock_idx, core_id,
-				core_line);
+		if (diff)
+			env_bit_set(i, bitmap);
 	}
+
+	return 0;
+}
+
+static inline bool end_of_section(ocf_cache_line_t entries_in_page,
+		uint16_t bit, uint16_t bitmap_size)
+{
+	if (bitmap_size < entries_in_page &&
+			DIV_ROUND_UP(bit+1, COMPARE_AT_ONCE) ==
+			DIV_ROUND_UP(entries_in_page, COMPARE_AT_ONCE)) {
+		return true;
+	}
+	return false;
+}
+*/
+
+static int update_collision(ocf_cache_t cache, struct ocf_metadata_raw *raw,
+		void *buffer, ctx_data_t *data, uint64_t overlap_start_data,
+		uint64_t overlap_page, uint64_t overlap_count)
+{
+	ocf_cache_line_t cache_etries = ocf_metadata_collision_table_entries(cache);
+	ocf_cache_line_t cache_line_count, cache_line_range_start;
+	struct ocf_metadata_ctrl *ctrl = cache->metadata.priv;
+	size_t data_size_on_page;
+	int i, j;
+	int result;
+
+	/* The range of cachelines with potentially updated collision section */
+	cache_line_range_start = overlap_page * raw->entries_in_page;
+	/* The last page of collision section may contain fewer entries than
+	   entries_in_page */
+	cache_line_count = OCF_MIN(cache_etries - cache_line_range_start,
+			raw->entries_in_page * overlap_count);
+
+	ctx_data_seek(cache->owner, data, ctx_data_seek_begin,
+			PAGES_TO_BYTES(overlap_start_data));
+	for (i = 0 ; i < overlap_count; i++) {
+		ocf_cache_line_t cline = (overlap_page + i)*raw->entries_in_page;
+		uint16_t bitmap_size;
+		uint64_t skip_bitmap[6] = {0};
+
+		ENV_BUG_ON(ARRAY_SIZE(skip_bitmap)*64 < raw->entries_in_page);
+
+		/* Handle the last page of the collision section */
+		if (cline + raw->entries_in_page > raw->entries) {
+			bitmap_size = raw->entries % raw->entries_in_page;
+			data_size_on_page = raw->entry_size * bitmap_size;
+		} else {
+			bitmap_size = raw->entries_in_page;
+			data_size_on_page = raw->entry_size * raw->entries_in_page;
+		}
+
+		/* Read new metadata */
+		ctx_data_rd_check(cache->owner, buffer, data, data_size_on_page);
+
+		for (j = 0; j < bitmap_size; j++) {
+			const struct ocf_metadata_map *old_mapping, *new_mapping;
+			int diff;
+
+			cline = cache_line_range_start + i * raw->entries_in_page + j;
+
+			old_mapping = ocf_metadata_raw_rd_access(cache,
+					&(ctrl->raw_desc[metadata_segment_collision]), cline);
+			new_mapping = buffer + j * raw->entry_size;
+
+			result = env_memcmp(old_mapping, raw->entry_size,
+					new_mapping, raw->entry_size, &diff);
+			if (unlikely(result))
+				return result;
+
+			/* Mapping hasn't changed */
+			if (!diff) {
+				env_bit_set(j, skip_bitmap);
+				continue;
+			}
+
+			/* The cache line has been inavlid, no need to invalidate it */
+			if (ocf_metadata_get_partition_id(cache, cline) == PARTITION_FREELIST)
+				continue;
+
+			cleanup_old_mapping(cache, cline, old_mapping->core_line,
+					old_mapping->core_id);
+		}
+
+		ctx_data_seek(cache->owner, data, ctx_data_seek_begin,
+				PAGES_TO_BYTES(overlap_start_data+i));
+		ocf_metadata_raw_update(cache, raw, data, overlap_page + i, 1);
+
+		j = 0;
+		while (j < bitmap_size) {
+			uint32_t len;
+			if (skip_bitmap[j/64] == 0xffffffffffffffff) {
+				j += 64;
+				continue;
+			}
+
+			/* Mapping for at least one cache line need to be updated */
+			for (len = j+64; j < len && j < bitmap_size; j++) {
+				if (env_bit_test(j, skip_bitmap))
+					continue;
+
+				cline = cache_line_range_start + i * raw->entries_in_page + j;
+
+				update_list_segment(cache, cline);
+			}
+
+		}
+	}
+
+	return 0;
 }
 
 static int passive_io_resume(struct ocf_request *req)
@@ -231,15 +346,13 @@ static int passive_io_resume(struct ocf_request *req)
 	uint64_t io_pages_count = BYTES_TO_PAGES(io->bytes);
 	uint64_t io_end_page = io_start_page + io_pages_count - 1;
 	ocf_end_io_t io_cmpl = req->master_io_req;
-	ocf_cache_line_t cache_etries = ocf_metadata_collision_table_entries(cache);
 	enum ocf_metadata_segment_id update_segments[] = {
 		metadata_segment_sb_config,
 		metadata_segment_collision,
 	};
-	int i;
+	int i, result = 0;
 
 	for (i = 0; i < ARRAY_SIZE(update_segments); i++) {
-		ocf_cache_line_t cache_line_count, cache_line_range_start;
 		enum ocf_metadata_segment_id seg = update_segments[i];
 		struct ocf_metadata_raw *raw = &(ctrl->raw_desc[seg]);
 		uint64_t raw_start_page = raw->ssd_pages_offset;
@@ -256,37 +369,22 @@ static int passive_io_resume(struct ocf_request *req)
 		overlap_page = overlap_start - raw_start_page;
 		overlap_count = overlap_end - overlap_start + 1;
 
-		if (seg == metadata_segment_collision) {
-			/* The range of cachelines with potentially updated collision
-			   section */
-			cache_line_range_start = overlap_page * raw->entries_in_page;
-			cache_line_count = raw->entries_in_page * overlap_count;
-
-			/* The last page of collision section may contain fewer entries than
-			   entries_in_page */
-			cache_line_count = OCF_MIN(cache_etries - cache_line_range_start,
-					cache_line_count);
-
-			/* The collision is not updated yet but the range of affected
-			   cachelines is already known. Remove the old mapping related info
-			   from the metadata */
-			cleanup_old_mapping(cache, cache_line_range_start,
-					cache_line_count);
+		if (seg != metadata_segment_collision) {
+			ctx_data_seek(cache->owner, data, ctx_data_seek_begin,
+					PAGES_TO_BYTES(overlap_start_data));
+			ocf_metadata_raw_update(cache, raw, data, overlap_page, overlap_count);
+			continue;
 		}
 
-		ctx_data_seek(cache->owner, data, ctx_data_seek_begin,
-				PAGES_TO_BYTES(overlap_start_data));
-		ocf_metadata_raw_update(cache, raw, data, overlap_page, overlap_count);
-
-		if (seg != metadata_segment_collision)
-			continue;
-
-		update_list_segment(cache, cache_line_range_start,
-				cache_line_count);
+		result = update_collision(cache, raw, req->priv, data,
+				overlap_start_data, overlap_page, overlap_count);
+		if (result)
+			goto end;
 	}
 
+end:
 	ocf_pio_async_unlock(req->cache->standby.concurrency, req);
-	io_cmpl(io, 0);
+	io_cmpl(io, result);
 	env_allocator_del(cache->standby.allocator, req);
 	return 0;
 }
@@ -300,6 +398,11 @@ static void passive_io_page_lock_acquired(struct ocf_request *req)
 {
 	ocf_engine_push_req_front(req, true);
 }
+
+/* Only one bit per page is required. Since `alock_status` has `uint8_t*`
+   type, one entry can carry status for 8 pages. */
+#define ALOCK_STATUS_SIZE \
+	OCF_DIV_ROUND_UP(BYTES_TO_PAGES(MAX_PASSIVE_IO_SIZE), 8)
 
 int ocf_metadata_passive_update(ocf_cache_t cache, struct ocf_io *io,
 		ocf_end_io_t io_cmpl)
@@ -353,6 +456,7 @@ int ocf_metadata_passive_update(ocf_cache_t cache, struct ocf_io *io,
 	req->core_line_first = io_start_page;
 	req->core_line_count = io_end_page - io_start_page;
 	req->alock_status = (uint8_t*)&req->map;
+	req->priv = (uint8_t*)&req->map + ALOCK_STATUS_SIZE;
 
 	lock = ocf_pio_async_lock(req->cache->standby.concurrency,
 			req, passive_io_page_lock_acquired);
@@ -374,9 +478,9 @@ int ocf_metadata_passive_io_ctx_init(ocf_cache_t cache)
 	size_t element_size, header_size, size;
 
 	header_size = sizeof(struct ocf_request);
-	/* Only one bit per page is required. Since `alock_status` has `uint8_t*`
-	   type, one entry can carry status for 8 pages. */
-	element_size = OCF_DIV_ROUND_UP(BYTES_TO_PAGES(MAX_PASSIVE_IO_SIZE), 8);
+	element_size = ALOCK_STATUS_SIZE;
+	/* Space for a buffer for reading IOs data */
+	element_size += PAGE_SIZE;
 	size = header_size + element_size;
 
 	cache->standby.allocator = env_allocator_create(size, name, true);
